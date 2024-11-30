13c0ec87adb378c36b5fc01751cc83544c2c3ecf
Faith Wanjiku <wanjikuf141@gmail.com>
2024-12-01 01:17:11 +0300

    commit

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 
+
 function scuberGreetingForFeet(distance) {
   // Write your code here!
   if (distance <= 400) {
-    return "This one is on me!";
+    return 'This one is on me!';
+  } else if (distance > 400 && distance <= 2000) {
+    return 'That will be twenty bucks.';
   } else if (distance > 2000 && distance <= 2500) {
-    return "I will gladly take your thirty bucks.";
-  } else if (distance > 2500) {
-    return "No can do.";
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
   }
 }
-
 
 
 function ternaryCheckCity(city) {
